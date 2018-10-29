97115ee4139df3e618db0de49fbf6320f3f36e4e
Elijah <elijahsteres@gmail.com>
2018-10-29 12:57:21 -0400

    Give ShapeType a toString method

--- src/animation/model/ShapeType.java
+++ src/animation/model/ShapeType.java
@@ -4,5 +4,16 @@ package animation.model;
  * An enum of the types of shapes that can be represented.
  */
 public enum ShapeType {
-  RECTANGLE, ELLIPSE
+  RECTANGLE("rectangle"), ELLIPSE("ellipse");
+
+  String type;
+
+  ShapeType(String type) {
+    this.type = type;
+  }
+
+  @Override
+  public String toString() {
+    return type;
+  }
 }

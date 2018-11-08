c0c7e33cdcde293c7a946d1715fd28ab24cf6935
MarkSavage11 <savagemark7@gmail.com>
2018-11-08 16:03:49 -0500

    Basic View Tests
    Adds a basic test for proper display for the textual and svg views

---
+++ test/Hw06Tests.java
@@ -0,0 +1,88 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import cs3500.animator.model.Animation;
+import cs3500.animator.model.AnimationModel;
+import cs3500.animator.model.AnimationModelImpl;
+import cs3500.animator.model.BasicAnimation;
+import cs3500.animator.model.ShapeType;
+import cs3500.animator.model.StateImpl;
+import cs3500.animator.view.AnimationView;
+import cs3500.animator.view.SVGAnimationView;
+import cs3500.animator.view.TextAnimationView;
+
+import static org.junit.Assert.assertEquals;
+
+public class Hw06Tests {
+
+  AnimationModel model;
+
+  @Before
+  public void init() {
+    this.model = new AnimationModelImpl(new Point(200, 200), new Dimension(600,600));
+    model.addShape("R", ShapeType.RECTANGLE);
+    model.addShape("C", ShapeType.ELLIPSE);
+    Animation animR1 =
+            new BasicAnimation(1, new StateImpl(new Point(200, 200),
+                    new Dimension(50, 100), Color.RED), 10,
+                    new StateImpl(new Point(200, 200), new Dimension(50, 100), Color.RED));
+    Animation animR2 =
+            new BasicAnimation(10, new StateImpl(new Point(200, 200),
+                    new Dimension(50, 100), Color.RED), 50,
+                    new StateImpl(new Point(300, 300), new Dimension(50, 100), Color.RED));
+
+    Animation animC1 =
+            new BasicAnimation(6, new StateImpl(new Point(440, 70),
+                    new Dimension(120, 60), Color.BLUE),20,
+                    new StateImpl(new Point(440, 70), new Dimension(120, 60), Color.GREEN));
+    Animation animC2 =
+            new BasicAnimation(20, new StateImpl(new Point(440, 70),
+                    new Dimension(120, 60), Color.GREEN), 50,
+                    new StateImpl(new Point(440, 250), new Dimension(60, 0), Color.BLACK));
+    model.addAnimation("R", animR1);
+    model.addAnimation("R", animR2);
+    model.addAnimation("C", animC1);
+    model.addAnimation("C", animC2);
+  }
+
+  @Test
+  public void testTextView(){
+    Appendable ap = new StringBuilder();
+    AnimationView view = new TextAnimationView(ap);
+    view.display(model);
+
+    assertEquals("canvas 200 200 600 600\n"
+            + "shape R rectangle\n"
+            + "motion R 1 200 200 50 100 255 0 0 10 200 200 50 100 255 0 0\n"
+            + "motion R 10 200 200 50 100 255 0 0 50 300 300 50 100 255 0 0\n"
+            + "shape C ellipse\n"
+            + "motion C 6 440 70 120 60 0 0 255 20 440 70 120 60 0 255 0\n"
+            + "motion C 20 440 70 120 60 0 255 0 50 440 250 60 0 0 0 0", ap);
+  }
+
+  @Test
+  public void testSVGView(){
+    Appendable ap = new StringBuilder();
+    AnimationView view = new SVGAnimationView(ap, 1);
+    view.display(model);
+
+    assertEquals("<?xml version=\"1.0\" standalone=\"no\"?>\n"
+            + "<svg viewBox=\"0 0 300 100\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+            + "<rect id=\"R\" x=\"200\" y=\"200\" width=\"50\" height=\"100\" fill=\"rbg(255,0,0)\">\n"
+            + "<animate attributeType=\"XML\" begin=\"10s\" dur=\"40s\" attributeName=\"x\" from=\"200\" to=\"300\"/>\n"
+            + "<animate attributeType=\"XML\" begin=\"10s\" dur=\"40s\" attributeName=\"y\" from=\"200\" to=\"300\"/>\n"
+            + "</rect>\n"
+            + "<ellipse id=\"C\" cx=\"400\" cy=\"70\" rx=\"120\" ry=\"60\" fill=\"rbg(0,0,255)\">\n"
+            + "<animate attributeType=\"XML\" begin=\"6s\" dur=\"14s\" attributeName=\"fill\" from=\"rgb(0,0,255)\" to=\"rgb(0,255,0)\"/>\n"
+            + "<animate attributeType=\"XML\" begin=\"20s\" dur=\"30s\" attributeName=\"cy\" from=\"70\" to=\"250\"/>\n"
+            + "<animate attributeType=\"XML\" begin=\"20s\" dur=\"30s\" attributeName=\"rx\" from=\"120\" to=\"60\"/>\n"
+            + "<animate attributeType=\"XML\" begin=\"20s\" dur=\"30s\" attributeName=\"ry\" from=\"60\" to=\"0\"/>\n"
+            + "<animate attributeType=\"XML\" begin=\"20s\" dur=\"30s\" attributeName=\"fill\" from=\"rgb(0,255,0)\" to=\"rgb(0,0,0)\"/>\n"
+            + "</ellipse>\n"
+            + "</svg>", ap);
+  }
+
+
+}

78dd07091a73e24da9d261a9273f958bc9c85926
MarkSavage11 <savagemark7@gmail.com>
2018-12-04 18:40:03 -0500

    Error handling for add and remove
    Implemented handling for add and remove errors where the user gives bad
    arguments.

--- src/cs3500/animator/view/AdaptedAnimationListener.java
+++ src/cs3500/animator/view/AdaptedAnimationListener.java
@@ -9,20 +9,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import cs3500.animator.controller.IAnimationController;
-import cs3500.animator.model.AnimationModel;
 import cs3500.animator.model.State;
 import cs3500.animator.model.StateImpl;
+import cs3500.animator.provider.model.ImmutableModel;
 import cs3500.animator.provider.view.IActionListener;
 import cs3500.animator.provider.view.IAnimateView;
 
 public class AdaptedAnimationListener implements IActionListener {
   private IAnimationController controller;
   private IAnimateView editor;
-  private AnimationModel model;
+  private ImmutableModel model;
 
 
   public AdaptedAnimationListener(IAnimationController controller,
-                                  IAnimateView editor, AnimationModel model){
+                                  IAnimateView editor, ImmutableModel model){
     this.controller = controller;
     this.editor = editor;
     this.model = model;
@@ -53,26 +53,42 @@ public class AdaptedAnimationListener implements IActionListener {
       case "Add":
         fields = editor.getAddFields("Add");
         String name = fields.get(8);
-
-        int tick = Integer.parseInt(fields.get(0)); //TODO: Check our inputs
-        State state = new StateImpl(
-                new Point(Integer.parseInt(fields.get(1)), Integer.parseInt(fields.get(2))),
-                new Dimension(Integer.parseInt(fields.get(3)), Integer.parseInt(fields.get(4))),
-                new Color(Integer.parseInt(fields.get(5)), Integer.parseInt(fields.get(6)),
-                        Integer.parseInt(fields.get(7))));
-        //if() TODO: Check how to add a shape and figure that shit out.
-        this.controller.addKeyframe(name, tick, state);
+        int addTick;
+        try{
+          addTick = Integer.parseInt(fields.get(0));
+        }catch(NumberFormatException e){
+          editor.showErrorMessage("Please enter a number for Tick");
+          break;
+        }
+        State state = null;
+        try {
+          state = new StateImpl(
+                  new Point(Integer.parseInt(fields.get(1)), Integer.parseInt(fields.get(2))),
+                  new Dimension(Integer.parseInt(fields.get(3)), Integer.parseInt(fields.get(4))),
+                  new Color(Integer.parseInt(fields.get(5)), Integer.parseInt(fields.get(6)),
+                          Integer.parseInt(fields.get(7))));
+        }catch(IllegalArgumentException e){
+          editor.showErrorMessage(e.getMessage());
+          break;
+        }
+        this.controller.addKeyframe(name, addTick, state);
         break;
       case "Remove":
         fields = editor.getAddFields("Remove");
-        //TODO: Check input
-        this.controller.deleteKeyFrame(fields.get(0), Integer.parseInt(fields.get(1)));
+        int removeTick;
+        try{
+          removeTick = Integer.parseInt(fields.get(1));
+        }catch(NumberFormatException e){
+          editor.showErrorMessage("Please enter a number for Tick");
+          break;
+        }
+        this.controller.deleteKeyFrame(fields.get(0), removeTick);
         break;
       case "Save":
         fields = editor.getAddFields("Save");
         if (fields.get(0).equals("Text") || fields.get(0).equals("text")) {
           try {
-            new SVGAnimationView(new BufferedWriter(new FileWriter(fields.get(1))), 30); //Can't get access to the speed in the editor
+            new SVGAnimationView(new BufferedWriter(new FileWriter(fields.get(1))), 30);
           }catch(IOException e){
             editor.showErrorMessage(e.getMessage());
           }
